405a64c78a6a860ef11774412fc1a5010f994c15
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-06-14 16:37:39 +0200

    feat(completion): handle prefix

--- internal/handler/completion.go
+++ internal/handler/completion.go
@@ -25,14 +25,30 @@ func (h *handler) handleTextDocumentCompletion(ctx context.Context, reply jsonrp
 	if err != nil {
 		return replyErr(ctx, reply, err)
 	}
-	text := strings.TrimSuffix(strings.TrimSpace(token.Text), ".")
-	slog.Info("completion", "text", text)
+	text := strings.TrimSpace(token.Text)
 
-	pkg := lookupPkg(text)
+	// Extract pkg from text
+	var (
+		pkgName = text
+		prefix  string
+	)
+	if i := strings.IndexByte(text, '.'); i > 0 {
+		pkgName = text[:i]
+		if i != len(text)-1 {
+			prefix = text[i+1:]
+		}
+	}
+	slog.Info("completion", "text", text, "pkgName", pkgName, "prefix", prefix)
+
+	pkg := lookupPkg(pkgName)
 	if pkg != nil {
 		for _, s := range pkg.Symbols {
 			if s.Recv != "" {
-				// skip symbols with receivers
+				// skip symbols with receiver (methods)
+				continue
+			}
+			if prefix != "" && !strings.HasPrefix(s.Name, prefix) {
+				// skip symbols that doesn't match the prefix
 				continue
 			}
 			items = append(items, protocol.CompletionItem{
